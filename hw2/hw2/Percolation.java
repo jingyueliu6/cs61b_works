720c9256e84f07d463e457752c9d6e0cca72cf6f
jingyueliu6 <jingyueliu1206@gmail.com>
2022-06-16 15:52:03 +0200

    hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,104 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int N;
+    private boolean [][] arr;
+    private int [][] arrNum;
+    private WeightedQuickUnionUF QU;
+    private int count;
+    private int top;
+    private int bottom;
+
+
+    private int helper(int row, int col) {
+        return row * N + col;
+    }
+
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+        arr = new boolean[N][N];
+        arrNum = new int[N][N];
+        count = 0;
+        top = N*N;
+        bottom = N*N +1;
+        this.N = N;
+        QU = new WeightedQuickUnionUF(N*(N+2));
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = false;
+                arrNum[i][j] = helper(i, j);
+            }
+        }
+
+        for (int j = 0; j < N; j++) {
+            QU.union(arrNum[0][j], top);
+            QU.union(arrNum[N-1][j], bottom);
+        }
+    }
+
+    private void argumentChecker(int row, int col) {
+        if (row >= 0 && row < N && col >= 0 && col < N) {
+            return;
+        }
+        throw new IndexOutOfBoundsException();
+    }
+
+
+    public void open(int row, int col) {
+        argumentChecker(row, col);
+        if (!isOpen(row, col)) {
+            arr[row][col] = true;
+            count += 1;
+            if (row-1 >=0 && isOpen(row-1, col)) {
+                QU.union(arrNum[row-1][col], arrNum[row][col]);
+            }
+            if (row+1 < N && isOpen(row+1, col)) {
+                QU.union(arrNum[row+1][col], arrNum[row][col]);
+            }
+            if (col-1 >=0 && isOpen(row, col-1)) {
+                QU.union(arrNum[row][col-1], arrNum[row][col]);
+            }
+            if (col+1 < N && isOpen(row, col+1)) {
+                QU.union(arrNum[row][col+1], arrNum[row][col]);
+            }
+        }
+
+    }
+
+    public boolean isOpen(int row, int col) {
+        argumentChecker(row, col);
+        return arr[row][col];
+    }
+
+    public boolean isFull(int row, int col) {
+        argumentChecker(row, col);
+        if (!isOpen(row, col)) {
+            return false;
+        }
+        return QU.connected(arrNum[row][col], top);
+    }
+
+    public int numberOfOpenSites()
+    {
+        return count;
+    }
+
+    public boolean percolates() {
+        return QU.connected(top, bottom);
+    }
+
+    public static void main (String[] args) {
+        Percolation sample = new Percolation(5);
+        sample.open(3, 4);
+        sample.open(2, 4);
+        sample.open(2, 2);
+        sample.open(2, 3);
+        sample.open(0, 2);
+        sample.open(1, 2);
+        System.out.println(sample.isFull(2, 2));
+        System.out.println(sample.percolates());
+    }
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,53 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
+public class PercolationStats {
+    private int T;
+    private Percolation[] samples;
+    private static double[] x;
+
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.T = T;
+        samples = new Percolation[T];
+        x = new double[T];
+        for (int i = 0; i < T; i++) {
+            samples[i] = pf.make(N);
+            while(!samples[i].percolates()) {
+                int row = StdRandom.uniform(N);
+                int col = StdRandom.uniform(N);
+                if (!samples[i].isOpen(row, col)) {
+                    samples[i].open(row, col);
+                }
+            }
+            x[i] = ((double) samples[i].numberOfOpenSites()) / ((double) (N * N));
+        }
+    }
+    public double mean() {
+        double sum = 0.0;
+        for (double frac : x) {
+            sum += frac;
+        }
+        return sum/ ((double) (T));
+    }
+
+    public double stddev() {
+        double mean = mean();
+        double s = 0.0;
+        for (double frac: x) {
+            s += (frac - mean) * (frac - mean);
+        }
+        return Math.sqrt(s/((double) (T - 1)));
+    }
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev()/ Math.sqrt((double) (T));
+    }
+    public double confidenceHigh() {
+        return mean() + 1.96 *stddev()/ Math.sqrt((double) (T));
+    }
 }
